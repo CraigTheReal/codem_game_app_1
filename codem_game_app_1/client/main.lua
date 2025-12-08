51cfa42c95406686174dc7c8cd16d767f3b6abe2
Craig_The_Real <notife19@gmail.com>
2025-12-08 02:29:11 +0100

    Add files via upload

--- codem_game_app_1/client/main.lua
+++ codem_game_app_1/client/main.lua
@@ -1,23 +1,43 @@
-
-
 local appRegistered = false
 
+RegisterNetEvent('codem-phone:phoneLoaded')
+AddEventHandler('codem-phone:phoneLoaded', function()
+    Wait(2000)
+    LoadPhoneApp()
+end)
 
-CreateThread(function()
-
-    while GetResourceState('codem-phone') ~= 'started' do
-        Wait(100)
+AddEventHandler('onResourceStart', function(resourceName)
+    if (resourceName == GetCurrentResourceName()) then
+        Wait(2000)
+        LoadPhoneApp()
     end
+end)
 
-    Wait(1000)
-
-    local htmlContent = LoadResourceFile(GetCurrentResourceName(), 'ui/index.html')
-
-    if not htmlContent then
+function LoadPhoneApp()
+    if appRegistered then
+        print('^3[RPS-GAME] App already registered, skipping...^7')
         return
     end
 
-  
+    while GetResourceState('codem-phone') ~= 'started' do
+        print('^3[RPS-GAME] Waiting for codem-phone to start...^7')
+        Wait(100)
+    end
+
+    -- Small delay
+    Wait(1000)
+
+    -- Read HTML content
+    local htmlContent = LoadResourceFile(GetCurrentResourceName(), 'ui/index.html')
+
+    if not htmlContent then
+        print('^1[RPS-GAME] Failed to load HTML content^7')
+        return
+    end
+
+    print('^3[RPS-GAME] Attempting to register app...^7')
+
+    -- Register the app
     local success, err = exports['codem-phone']:AddCustomApp({
         identifier = 'rps-game',
         name = 'RPS Game',
@@ -26,19 +46,28 @@ CreateThread(function()
         description = 'Rock Paper Scissors multiplayer betting game',
         defaultApp = false,
         notification = true,
+        job = {
+            -- ['police'] = { 3, 4 },
+            -- ['ambulance'] = { 2, 3 }
+        },
         onOpen = function()
+            print('[RPS-GAME] RPS Game app opened')
             SendNUIMessage({
                 type = 'appOpened'
             })
         end,
         onClose = function()
+            print('[RPS-GAME] RPS Game app closed')
         end
     })
 
     if success then
         appRegistered = true
+        print('^2[RPS-GAME] RPS Game app registered successfully!^7')
+    else
+        print('^1[RPS-GAME] Failed to register RPS Game app: ' .. tostring(err) .. '^7')
     end
-end)
+end
 
 
 RegisterNetEvent('codem-game:updateLobby')
